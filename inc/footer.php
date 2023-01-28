5dcfe2abc72c9d6cd8d20a0b197321bbfbfca1bf
sr-sohan <sr.sohanur22@gmail.com>
2023-01-28 09:41:04 +0600

    inc folder add

---
+++ inc/footer.php
@@ -0,0 +1,76 @@
+
+    <!-- footer area start  -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6 col-md-3">
+                    <div class="single-footer">
+                        <h3>information</h3>
+                        <hr>
+                        <ul>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="single-footer">
+                        <h3>information</h3>
+                        <hr>
+                        <ul>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="single-footer">
+                        <h3>information</h3>
+                        <hr>
+                        <ul>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                            <li><i class="fa-solid fa-caret-right"></i><a href="">About us</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-sm-6 col-md-3">
+                    <div class="single-footer">
+                        <h3>Contact</h3>
+                        <hr>
+                        <ul>
+                            <li><i class="fa-solid fa-location-dot"></i> <span>Genuity Systems,Kazipara,Mipur - Dhaka.</span></li>
+                            <li><i class="fa-solid fa-mobile-screen-button"></i> <span>+8801302460356</span> <br> <span>+8801302460356</span></li>
+                            <li><i class="fa-solid fa-envelope"></i><span>sohanur.rahman1522@gmail.com</span></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <div class="copyright text-center py-3">
+        <p>Copyright by : Genuity Systems</p>
+    </div>
+    <!-- footer area end -->
+
+
+
+
+
+
+
+    <!-- bootstrap js  -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js"></script>
+    <!-- main js  -->
+    <script src="assets/js/main.js"></script>
+</body>
+
+</html>
---
+++ inc/header.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>BLAZE-UP</title>
+
+    <!-- font awesome css  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <!-- bootstrap css  -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css">
+
+    <!-- main css  -->
+    <link rel="stylesheet" href="assets/css/style.css">
+</head>
+
+<body>
+
+
+    <!-- header part area start  -->
+    <header>
+        <div class="container">
+            <div class="header-content py-2">
+                <div class="logo-part">
+                    <a href=""><h1> <span>Blaze</span>-up</h1></a>
+                </div>
+                <div class="cart-part d-flex align-items-center">
+
+                    <a href="" type="button" class="btn  position-relative cart-icon">
+                        <i class="fa-solid fa-cart-shopping"></i>
+                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-full cart-badge shadow-sm">
+                            13
+                        </span>
+                    </a>
+                    <p><a class="btn ms-3 mt-3 login-btn" href="">Login</a></p>
+
+                </div>
+            </div>
+        </div>
+    </header>
+    <!-- header part area end  -->
+
+    <!-- navbar area start  -->
+    <nav>
+        <div class="container">
+            <div class="nav-wrap">
+                <ul class="nav-area-item">
+                    <li><a href="">Smart phone</a></li>
+                    <li><a href="">Desktop</a></li>
+                    <li><a href="">laptop</a></li>
+                    <li><a href="">Networking</a></li>
+                    <li><a href="">software</a></li>
+                    <li><a href="">accessories</a></li>
+                </ul>
+                <form action="" class="search-box">
+                    <input type="search" name="" id="" placeholder="Search your item...">
+                    <button type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    <!-- navbar area end -->
